94579fa73f4a9fd7374bc710f6d21a180476fd14
Disath Liyanage <disathsenitha@gmail.com>
2026-06-25 16:54:42 +0530

    fix 3

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,15 @@ import PhotoCarousel from "@/components/PhotoCarousel";
 import Reviews from "@/components/Reviews";
 import TreeHouseSection from "@/components/TreeHouseSection";
 import getDiscoveredImages from "@/lib/getImageFiles";
-import getReviews from "@/lib/getReviews";
 
 export default async function Page() {
   const images = getDiscoveredImages();
   const preferredHeroImage = "/images/outdoor/Front.jpeg";
-  const heroImage = images.includes(preferredHeroImage) ? preferredHeroImage : images[0];
+  const heroImage = images.includes(preferredHeroImage)
+    ? preferredHeroImage
+    : images[0];
+
   const treeHouseImage = images[1] ?? images[0];
-  const reviewData = await getReviews();
 
   return (
     <main className="bg-[#F5F0E8] text-[#1F2A20]">
@@ -21,11 +22,7 @@ export default async function Page() {
       <Hero imageSrc={heroImage} />
       <PhotoCarousel images={images} />
       <TreeHouseSection imageSrc={treeHouseImage} />
-      <Reviews
-        reviews={reviewData.reviews}
-        overallRating={reviewData.overallRating}
-        reviewCount={reviewData.totalRatings}
-      />
+      <Reviews />
       <MapSection />
       <Footer />
     </main>
