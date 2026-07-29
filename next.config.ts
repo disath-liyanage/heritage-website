588eaf9611806503cb32d6b4eeeda192c1d3f40c
Disath Liyanage <disathsenitha@gmail.com>
2026-07-29 07:57:43 +0530

    Photo ft fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gjgfkvdusrumcucgzrtt.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/gallery/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
